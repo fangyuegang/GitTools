32d7a8d624e2cf9e02be6689326c3d31b35e096e
fangyuegang <34093207+fangyuegang@users.noreply.github.com>
2021-05-06 20:40:24 +0800

    Update gittool.sh
    Modify  cannot  find  build.sh file

--- gittool.sh
+++ gittool.sh
@@ -212,10 +212,18 @@ elif [ $num == 3 ];
 		echo $ini_select:$i
 	done
 	read -p "请输入您的选择:" VersionSelect
-	echo $ini_select
 	if [ $VersionSelect -lt $ini_select ];
 		then 
 		echo "你当前选择的版本:"${arr_name[$VersionSelect]}
+		for ((i=0;i<20;i++))
+		do 
+			repo_path=`find . -maxdepth 1 -name '.repo'`
+			if [ -n "$repo_path" ];
+				then 
+				break
+			fi
+			cd ../
+		done
 		echo 1.选择编译部分镜像
 		echo 2.选择编译所有镜像
 		echo 3.退出
